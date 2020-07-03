430a49565e09e54aa76592f015a82c6e7a62edfa
emanuele5180 <macina5180>
2020-07-03 15:50:19 +0200

    classe active rimossa da img

---
+++ main.js
@@ -0,0 +1,11 @@
+//devo fare in modo che al click sulla freccia destra l'immagine cambi, quindi devo aggiungere e togliere una classe che al click mandi in display none l'immagine corrente e in display block l'immagine successiva.
+
+
+$(document).ready(function(){
+
+  $(".next").click(function(){
+    $(".images img").removeClass("active");
+    console.log(".next");
+  })
+
+})
